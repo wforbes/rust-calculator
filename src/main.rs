53e54b873a4c1a5f33df3e208a142848f75b73d7
Will Forbes <wforbes87@gmail.com>
2022-04-03 10:29:40 +0000

    add procedure for basic calculator

--- src/main.rs
+++ src/main.rs
@@ -16,4 +16,51 @@ fn main() {
   println!("{:?}", args1);
   println!("{:#?}", args1); // ':#?' is used to pretty-print
 
+  // Use args to perform add, subtract, multiply or divide on two numbers
+  //  printing the math expression with the result
+  
+  // declare another 'args' variable as mutable with 'mut'
+  let mut args2: Args = args();
+  //  accessing the first user argument requires skipping the
+  //  first run directory arg
+  let first = args2.nth(1).unwrap();
+  //  nth uses an internal iterator, so the next index after
+  //  the last unwrapped item will be the 0-th element
+  //  with the current iterator position
+  //  - we'd also like 'operator' to be type char
+  //    so we get an iterator on the unwrapped string with .chars()
+  //    then we use .next() to iterate on the first element in the string
+  //    and we .unwrap() that element, returning the char
+  let operator = args2.nth(0).unwrap().chars().next().unwrap();
+  let second = args2.nth(0).unwrap();
+
+  // the parse method parses the string into another type
+  //  we can use the variable type annotation like this
+  let first_number: f32 = first.parse().unwrap();
+  //  or we can use turbofish syntax on the parse method
+  let second_number = second.parse::<f32>().unwrap();
+
+  let result = operate(operator, first_number, second_number);
+  
+  println!("{}", output(first_number, operator, second_number, result));
+}
+
+// implicit return, without return keyword or semi-colon
+fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
+  if operator == '+' {
+    first_number + second_number
+  } else if operator == '-' {
+    first_number - second_number
+  } else if operator == '/' {
+    first_number / second_number
+  } else if operator == '*' {
+    first_number * second_number
+  } else {
+    0.0
+  }
+}
+
+fn output(first_number: f32, operator: char, second_number: f32, result: f32) -> String {
+  format!("{} {} {} = {}", first_number, operator, second_number, result)
+}
 }
